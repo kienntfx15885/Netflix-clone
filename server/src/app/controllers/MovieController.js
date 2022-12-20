a8e4d6b92d2cdd56c0bfc1a1ea74315d91fae046
kienntfx15885 <kienntfx15885@funix.edu.vn>
2022-12-20 17:06:54 +0700

    Update file

---
+++ server/src/app/controllers/MovieController.js
@@ -0,0 +1,5 @@
+const MovieController = {
+
+}
+
+module.exports = MovieController
--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,10 +1,13 @@
 const authRouter = require('./auth')
 const userRouter = require('./user')
+const movieRouter = require('./movie')
 
 function routes (app) {
     app.use('/api/auth', authRouter)
 
     app.use('/api/users', userRouter)
+
+    app.use('/api/movies', movieRouter)
 }
 
 module.exports = routes
